022cb4b7514ff658b8a4abe923dab3f1a9eb2b41
Younes Amajid <yamajid@e3r5p4.1337.m>
2024-12-11 15:52:15 +0100

    game initial sent success

--- backend/ping_pong/consumers.py
+++ backend/ping_pong/consumers.py
@@ -3,21 +3,21 @@ import json
 
 
 class Match:
-    def __init__(self, player_1, player_2, match_name):
-        self.player_1 = player_1
-        self.player_2 = player_2
-        self.match_name = match_name
-        
+    def __init__(self, player_1, player_2, group_name):
+        self.player1 = player_1
+        self.player2 = player_2
+        self.group_name = group_name
         self.is_active = True
 
     def players(self):
-        return [self.player1, self.player2, self.match_name]
+        return [self.player1, self.player2, self.group_name]
 
 class GameClient(AsyncWebsocketConsumer):
     
     
     connected_sockets = []
-    active_matches = []    
+    active_matches = []
+    num = 1 
     async def connect(self):
         self.player = {}
         await self.accept()
@@ -38,49 +38,44 @@ class GameClient(AsyncWebsocketConsumer):
         if len(self.connected_sockets) >= 2:
             player1 = self.connected_sockets.pop(0)
             player2 = self.connected_sockets.pop(0)
-            self.group_name = 'game1'
-            await self.channel_layer.group_add(self.group_name, player1['player_name'])
-            await self.channel_layer.group_add(self.group_name, player2['player_name'])
+            self.group_name = f'group_{len(self.active_matches)}'
             new_match = Match(player1, player2, self.group_name)
             self.active_matches.append(new_match)
+            await self.channel_layer.group_add(self.group_name, player1['player_name'])
+            await self.channel_layer.group_add(self.group_name, player2['player_name'])
             await self.channel_layer.group_send(self.group_name, # data{ type:"move", playerNumber: "1", groupName: "self.group_name" }
                 {
                     'type': 'game_started',
-                    'paddlesW': 700,
-                    'paddlesH': 400,
-                    'paddlesX': 1,
-                    'paddlesY': 70, 
-                    'paddlesS': 10,
+                    'paddlesW': 10, 
+                    'paddlesH': 100,
+                    'paddlesX': 5,
+                    'paddlesY': 200,
                     'paddlesB': 10,
-                    'paddlesSC': 0,
                     'game_group': self.group_name
                 })
         
         
-    async def game_started(self, event):
-        await self.send(json.dumps({
-            'type': event['type'],
-            'paddlesW': 700,
-            'paddlesH': 400,
-            'paddlesX': 1,
-            'paddlesY': 70, 
-            'paddlesS': 10,
-            'paddlesB': 10,
-            'paddlesSC': 0,
-            'game_group': event['game_group']
-            
-        }))
-        #Matches['game'] ={
-               
-        #}
         
-    # async def receive(self, text_data):
-    # if data.type == 'move':
-    #    
-    #     try:
-    #         data = json.loads(text_data)
-    #     except Exception as e:
-    #         print("error :", e)
+    async def receive(self, text_data):
+       
+        try:
+            data = json.loads(text_data)
+        except Exception as e:
+            print("error :", e)
+        # if data['type'] == 'paddleMove':
+        #     if data['playerNumber'] == '1':
+        #         group_name = data.gameGroup
+        #         await self.channel_layer.group_send(
+        #             group_name,
+        #             {
+        #                 'type': 'paddleMoved',
+        #                 'paddlew': data['paddlew'],
+        #                 'paddleh': data['paddleh'],
+        #                 'paddlex': data['paddlex'],
+        #                 'paddley': data['paddley'],
+        #                 'playerN': data['playerNumber']
+        #             }
+                # )
     
             
     # async def disconnect(self, close_data):
@@ -96,10 +91,28 @@ class GameClient(AsyncWebsocketConsumer):
     #         }
     #     )
         
+    # async def paddleMoved(self, event):
+    #     await self.send(json.dumps({
+    #         'type': event['type'],
+    #         'paddlesW': event['paddlew'],
+    #         'paddlesH': event['paddleh'],
+    #         'paddlesX': event['paddlex'],
+    #         'paddlesY': event['paddley'],
+    #         'playerN': event['playerN']
+            
+    #     }))
     
-    async def players_ready(self, event):
+    
+    async def game_started(self, event):
         await self.send(json.dumps({
             'type': event['type'],
+            'paddlesW': 10,
+            'paddlesH': 100,
+            'paddlesX': 5,
+            'paddlesY': 200,
+            'paddlesB': 10,
+            'game_group': event['game_group']
+            
         }))
 
     async def player_disconnected(self, event):
--- game/game/src/page.jsx
+++ game/game/src/page.jsx
@@ -6,7 +6,7 @@
 /*   By: yamajid <yamajid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 12:41:28 by momihamm          #+#    #+#             */
-/*   Updated: 2024/12/09 22:42:53 by yamajid          ###   ########.fr       */
+/*   Updated: 2024/12/11 15:47:31 by yamajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,36 +21,45 @@ function Back() {
   return <div className="background"></div>;
 }
 let y = 0
+let x = 0
+let height = 0
+let width = 0
+let bord = 0
 
 function App() {
   const [gameUrl] = useState('ws://127.0.0.1:8000/ws/ping_pong');
   const { sendMessage, lastMessage, readyState } = useWebSocket(gameUrl);
-  const leftPaddle = useRef('')
-  const rightPaddle = useRef('')
-  const [playN, setPlayerN] = useState('')
+  const [playerNumber, setPlayerNmber] = useState('')
+  // const [matchNumber, setMatchNmber] = useState('')
+  const [playerName, setPlayerName] = useState('')
   const [gameG, setGame] = useState('')
   
   useEffect(() => {
         if (readyState === WebSocket.OPEN){
           if (lastMessage != null){
             const data = JSON.parse(lastMessage.data);
-            console.log(data)
-            if (data['type'] === 'connection')
-              setPlayerN(data['information']['player_number'])
+            // console.log(data)
+            if (data['type'] === 'connection'){
+                setPlayerNmber(data['information']['player_number'])
+                setPlayerName(data['information']['player_name'])
+            }
             if (data['type'] === 'game_started')
-              leftPaddle.current.width = data['paddlesW']    
-              leftPaddle.current.height = data['paddlesH']    
-              leftPaddle.current.x = data['paddlesX']    
-              y = data['paddlesY']    
-              leftPaddle.current.speed = data['paddlesS']    
-              leftPaddle.current.bord = data['paddlesB']    
-              leftPaddle.current.score = data['paddlesSC']
+              console.log(data['matchNum'])
+              width = data['paddlesW']    
+              height = data['paddlesH']    
+              x = data['paddlesX']    
+              y = data['paddlesY']       
+              bord = data['paddlesB']   
+               
               setGame(data['game_group'])
+              console.log(data)
+              if (data['type'] === 'paddleMoved'){
+              }
             }
           }
         }, [lastMessage])
   const Canvas = () => {
-    let ball;
+    let ball, leftPaddle, rightPaddle;
     let paddleWidth = 0;//= p5.width * 0.02; // 2% of canvas width
     let paddleHeight = 0;// = p5.height * 0.2; // 20% of canvas height
     let ballRadius = 0;// p5.width * 0.02; // 2% of canvas width
@@ -73,8 +82,8 @@ function App() {
       canvas.style('left', '10%');         // Move 10% to the right
       canvas.style('border-radius', '15px');
       canvas.style('border', '2px dashed white');
-      leftPaddle.current = new Paddle(p5.width * 0.01 , p5.height * 0.4, paddleWidth, paddleHeight, 10, 10, initScore);
-      rightPaddle.current = new Paddle(p5.width * 0.99 - paddleWidth, p5.height * 0.4, paddleWidth, paddleHeight, 10, 10, initScore);
+      leftPaddle = new Paddle(p5.width * 0.01 , p5.height * 0.4, paddleWidth, paddleHeight, 10, 10, initScore);
+      rightPaddle = new Paddle(p5.width * 0.99 - paddleWidth, p5.height * 0.4, paddleWidth, paddleHeight, 10, 10, initScore);
       ball = new Ball(canvasWidth * 0.5, canvasHeight * 0.5, ballRadius, ballSpeed, 0, initAngle, canvasWidth, ballSpeed);
       p5.frameRate(60);
     };
@@ -83,20 +92,31 @@ function App() {
       // console.log("whach dkhel be3da");
       // Move left paddle with W (up) and S (down)
       if (p5.keyIsDown(87) ) { // 'W' key
-        console.log(playN)
-        console.log(gameG)
-        leftPaddle.current.y = Math.max(0, leftPaddle.current.y - leftPaddle.current.speed); // Prevent moving out of bounds
+        console.log(playerNumber)
+        // console.log(playerName)
+        // console.log(gameG)
+        leftPaddle.y = Math.max(0, leftPaddle.y - leftPaddle.speed); // Prevent moving out of bounds
+        // sendMessage(JSON.stringify({
+        //   'type' : 'paddleMove',
+        //   'playerNumber': playerNumber,
+        //   'playerName': playerName,
+        //   'paddlew': leftPaddle.width,
+        //   'paddleh': leftPaddle.height,
+        //   'paddlex': leftPaddle.x,
+        //   'paddley': leftPaddle.y,
+        //   'gameGroup': gameG
+        // }))
       }
       if (p5.keyIsDown(83)) { // 'S' key
-        leftPaddle.current.y = Math.min(p5.height - leftPaddle.current.height, leftPaddle.current.y + leftPaddle.current.speed);
+        leftPaddle.y = Math.min(p5.height - leftPaddle.height, leftPaddle.y + leftPaddle.speed);
       }
       
       // Move right paddle with UP and DOWN arrow keys
       if (p5.keyIsDown(p5.UP_ARROW)) {
-        rightPaddle.current.y = Math.max(0, rightPaddle.current.y - rightPaddle.current.speed); // Prevent moving out of bounds
+        rightPaddle.y = Math.max(0, rightPaddle.y - rightPaddle.speed); // Prevent moving out of bounds
       }
       if (p5.keyIsDown(p5.DOWN_ARROW)) {
-        rightPaddle.current.y = Math.min(p5.height - rightPaddle.current.height, rightPaddle.current.y + rightPaddle.current.speed);
+        rightPaddle.y = Math.min(p5.height - rightPaddle.height, rightPaddle.y + rightPaddle.speed);
       }
     };
     
@@ -118,13 +138,13 @@ function App() {
       p5.textSize(p5.width * 0.1); // Text size relative to canvas width
       p5.textAlign(p5.CENTER, p5.CENTER); // Center align text
       handlePaddleMovement(p5);
-      leftPaddle.current.show(p5);
-      rightPaddle.current.show(p5, leftPaddle.current.x, y, leftPaddle.current.width, leftPaddle.current.height, leftPaddle.current.bord);
+      leftPaddle.show(p5);
+      rightPaddle.show(p5, x, y, width, height, bord);
       
-      ball.move(p5, leftPaddle.current, rightPaddle.current);
+      ball.move(p5, leftPaddle, rightPaddle);
       // console.log(ball)
-      p5.text(leftPaddle.current.score, p5.width * 0.25, p5.height * 0.2); // Left score at 25% width
-          p5.text(rightPaddle.current.score, p5.width * 0.75, p5.height * 0.2); // Right score at 75% width
+      p5.text(leftPaddle.score, p5.width * 0.25, p5.height * 0.2); // Left score at 25% width
+          p5.text(rightPaddle.score, p5.width * 0.75, p5.height * 0.2); // Right score at 75% width
           ball.show(p5, );
         };
         
@@ -132,11 +152,11 @@ function App() {
           // Adjust the canvas size dynamically on window resize
       const canvasWidth = p5.windowWidth * 0.6; // 80% of window width
       const canvasHeight = p5.windowHeight * 0.5; // 60% of window height
-      leftPaddle.current.x = canvasWidth * 0.01; 
+      leftPaddle.x = canvasWidth * 0.01; 
       paddleWidth = canvasWidth * 0.01;
-      rightPaddle.current.x = canvasWidth * 0.99 - paddleWidth;
-      leftPaddle.current.width = canvasWidth * 0.01;
-      rightPaddle.current.width = canvasWidth * 0.01;
+      rightPaddle.x = canvasWidth * 0.99 - paddleWidth;
+      leftPaddle.width = canvasWidth * 0.01;
+      rightPaddle.width = canvasWidth * 0.01;
       ball.radius = canvasWidth * 0.02;;
       ball.x = canvasWidth * 0.5;
       ball.y = canvasHeight * 0.5;
